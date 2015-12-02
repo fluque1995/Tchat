9fbacc37c076557e754438e60fd4fbebbe10d3c3
Mariadel <mariadel52@correo.ugr.es>
2015-12-02 19:22:12 +0100

    directoryService

---
+++ report/directoryService.java
@@ -0,0 +1,31 @@
+public class DirectoryService{
+
+	private Map<String, String> users = new HashMap<String, String>();
+
+	public void connect(String name, String IP){
+			users.put(name,IP);
+	}
+
+	public void (String name){
+			users.remove(name);
+	}
+
+	public Map<String, String> getUssers(){
+			return users.values();
+	}
+
+	/*public BOOOOOM(){
+		System.out.println("Mariadel se caga en Paco corazon corazon");
+		users.clear();
+	}*/
+
+
+	public String isConnected(String name){
+		if(users.containsKey(name)){
+			return users.get(name);
+		}
+		else{
+			return "No conectado";
+		}
+	}
+}
