60a89d8b428f6cd48331fd2cc6f8f85def91d8bc
Mariadel <mariadel52@correo.ugr.es>
2015-12-03 20:25:00 +0100

    El speaker compila!

--- code/ClientSpeaker.java
+++ code/ClientSpeaker.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.io.Console;
+import java.net.InetAddress;
 
 public class ClientSpeaker{
 
@@ -25,34 +26,38 @@ public class ClientSpeaker{
 		String userIP;
 		String message;
 
+		try {
+			// Conexion with server
+			host = "tututututututututu".getBytes();
+			port = 2048;
+			direction = InetAddress.getByAddress(host);
+			socket = new Socket(direction, port);
 
-		// Conexion with server
-		host = "tututututututututu".getBytes(StandardCharsets.UTF_8);
-		port = 2048;
-		direction = InetAddress.getByAddress(host);
-		socket = new Socket(direction, port);
+			// Hand shaking
+			outPrinter = new PrintWriter(socket.getOutputStream(), true);
+			outPrinter.println("01");
+			inReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-		// Hand shaking
-		outPrinter = new PrintWriter(socket.getOutputStream(), true);
-		outPrinter.println("01");
-		inReader = new BufferedReader(new InputStreamReader(socketServicio.getInputStream()));
+			// Choose user to talk with
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			user = br.readLine();
+			outPrinter.println(user);
 
-		// Choose user to talk with
-		String user;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		outPrinter.println(user);
+			// Get user IP
+			userIP = inReader.readLine();
+			if(!userIP.equals("-1")){
+				socketConnection = new Socket(userIP, userPort);
+				outPrinter = new PrintWriter(socketConnection.getOutputStream(), true);
 
-		// Get user IP
-		userIP = inReader.readLine();
-		if(!userIP.equal("-1")){
-			socketConnection = new Socket(userIP, userPort);
-			outPrinter = new PrintWriter(socketConnection.getOutputStream(), true);
-
-			//Sending messages
-			while(true){
-				message = br.readLine();
-				outPrinter(message);
+				//Sending messages
+				while(true){
+					message = br.readLine();
+					outPrinter.println(message);
+				}
 			}
+		}
+		catch (IOException e) {
+			System.err.println("Error");
 		}
 	}
 }
